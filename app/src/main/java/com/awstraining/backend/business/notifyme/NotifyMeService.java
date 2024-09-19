ddc47a4b2e02df0ae9dfb7be1a076cd84303ac6d
mmigdal <mateusz.migdal@partner.bmwgroup.com>
2024-09-19 14:24:26 +0200

    missing autowired was added

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -1,5 +1,6 @@
 package com.awstraining.backend.business.notifyme;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,7 @@ public class NotifyMeService {
     //  1. Inject Translator
     // TODO lab3
     //  1. Inject sentiment detector
-//    @Autowired
+    @Autowired
     public NotifyMeService(MessageSender sender) {
         this.sender = sender;
     }
